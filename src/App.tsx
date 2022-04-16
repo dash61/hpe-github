81c90e245145309ee46b79da7881a849df020e8e
dash61 <drlilly12@gmail.com>
2022-04-16 12:53:33 -0400

    Getting repos, just listing names now

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import './App.css';
 import githubUsernameRegex from 'github-username-regex';
-import {getUserData, IGithubUser} from "./external/github";
+import {getUserData, getUserRepos, IGithubUser, IGithubRepo} from "./external/github";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import GithubUser from './components/GithubUser';
 
+// let reposData = false;
 
 function App() {
   const [userName, setUserName] = React.useState("");
   const [avatarUrl, setAvatarUrl] = React.useState("");
   const [location, setLocation] = React.useState("");
+  const [numRepos, setNumRepos] = React.useState(0);
+  const [repos, setRepos] = React.useState<IGithubRepo[]>([]);
+
+  // const renderHeader = () => {
+  //   let headerElement = ['id', 'name', 'email', 'phone', 'operation'];
+
+  //   return headerElement.map((key, index) => {
+  //       return <th key={index}>{key.toUpperCase()}</th>
+  //   });
+  // }
 
   const fetchUserData = async (url: string) => {
     const userData: IGithubUser | undefined = await getUserData(url);
@@ -20,7 +31,20 @@ function App() {
         console.log("user data object is NOT empty");
         setAvatarUrl(userData.avatar_url);
         setLocation(userData.location);
+        setNumRepos(userData.public_repos);
       }
+    }
+  }
+
+  const fetchUserRepos = async (url: string) => {
+    const userRepos: IGithubRepo[] = await getUserRepos(url);
+    if (userRepos.length) {
+      setRepos([...userRepos]);
+      // setRepoName(userRepos.name);
+      // setRepoDescr(userRepos.description);
+      // setRepoLang(userRepos.language);
+      // setRepoUrl(userRepos.url);
+      // setRepoStars(userRepos.stargazers_count);
     }
   }
 
@@ -31,6 +55,7 @@ function App() {
 
     if (okName) {
       fetchUserData(url[1]);
+      fetchUserRepos(url[1]);
       setUserName(url[1]);
     }
   }, []);
@@ -44,10 +69,18 @@ function App() {
             Error - invalid user name entered ({userName})
           </p>
         :
-        <>
-          <GithubUser avatarUrl={avatarUrl} userName={userName} location={location}/>
-        </>
-    }
+          <>
+            <GithubUser avatarUrl={avatarUrl} userName={userName} location={location}/>
+            { numRepos && repos.map((repo, index) => {
+                return (
+                  <div>
+                    {repo.name}
+                  </div>
+                )
+              })
+            }
+          </>
+        }
       </main>
 
       <Footer />
--- src/external/github.ts
+++ src/external/github.ts
@@ -1,10 +1,19 @@
 export interface IGithubUser {
   avatar_url: string;
-  name: string;
-  login: string;
-  url: string;
-  repos_url: string;
   location: string;
+  login: string;
+  name: string;
+  public_repos: number;
+  repos_url: string;
+  url: string;
+}
+
+export interface IGithubRepo {
+  description: string;
+  language: string;
+  name: string;
+  stargazers_count: number;
+  url: string;
 }
 
 export async function getUserData(user: string): Promise<IGithubUser | undefined> {
@@ -27,7 +36,7 @@ export async function getUserData(user: string): Promise<IGithubUser | undefined
   return undefined;
 }
 
-export async function getUserRepos(user: string): Promise<[]> {
+export async function getUserRepos(user: string): Promise<IGithubRepo[]> {
   console.log("going to get user=", user);
   
   const req: RequestInit = {
@@ -39,7 +48,7 @@ export async function getUserRepos(user: string): Promise<[]> {
   };
   const rsp = await fetch(`https://api.github.com/users/${user}/repos`, req);
   if (rsp.status === 200) {
-      const jsonResults: any = await rsp.json();
+      const jsonResults: IGithubRepo[] = await rsp.json();
 
       console.log("jsonResults=", jsonResults);
       return jsonResults; // as P[];
